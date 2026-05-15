61ca7875fb2d83775c01166702b4c7bbf3c69b05
Vinh <155561399+QuangVinh205@users.noreply.github.com>
2026-05-15 20:18:16 +0700

    Add project files.

---
+++ PhoneStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PhoneStore.DB;
+using PhoneStore.Models;
+
+namespace PhoneStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private PhoneStoreDbContext db;
+
+        public ProductController(PhoneStoreDbContext db) { this.db = db; }
+        public async Task<IActionResult> Index()
+        {
+            var products = await db.Products.Include(p => p.Category).ToListAsync();
+            return View(products);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var cats = await db.Categories.Select(c=> new SelectListItem(
+                c.Name,c.Id.ToString())).ToListAsync();
+            ViewBag.CategoryId = cats;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product p)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(p);
+            }
+
+            await db.Products.AddAsync(p);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
